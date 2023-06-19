240a9a88fcc79d05803bfd069bf7983caf815a6a
Max Ohsawa <max.ohsawa@gmail.com>
2023-06-19 17:16:34 -0400

    adds mongoDB connection, json and urlencoded middlewares, router

--- server/server.js
+++ server/server.js
@@ -1,10 +1,24 @@
+const path = require('path');
 const express = require('express');
 require('dotenv').config();
+
+const db = require('./db/connection');
+
+const routes = require('./routes');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-})
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, '..', 'client', 'public')));
+
+app.use('/', routes);
+
+db.once('open', () => {
+  console.log('DB connection established')
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+});
