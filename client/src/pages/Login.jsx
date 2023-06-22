d2c1382599c5b106fc13814f4d424caaf5cf981e
David Keim <dmk252@cornell.edu>
2023-06-22 11:55:03 -0400

    created basic login page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,10 +1,76 @@
-import { Box, Heading } from "@chakra-ui/react";
+import { Box, Heading, FormControl, FormLabel, Input, Button, Text } from "@chakra-ui/react";
+import { useState } from "react";
 
 function Login() {
+
+	const [emailInput, setEmailInput] = useState(" ");
+    const [passwordInput, setPasswordInput] = useState(" ");
+	let emailError = emailInput === '';
+	let passwordError = passwordInput === '';
+	let emailMessage = (emailError ? "Email is required" : "");
+	let passwordMessage = (passwordError ? "Password is required" : "");
+
+	const signIn = () => {
+		if (emailInput === ' ') {
+			emailError = true;
+		}
+		if (passwordInput === ' ') {
+			passwordError = true;
+		}
+		console.log("sign in attempt");
+		console.log("Email: " + emailInput);
+		console.log("Password: " + passwordInput);
+	}
+
+	const updateEmail = (event) => {
+		setEmailInput(event.currentTarget.value);
+	}
+
+	const updatePassword = (event) => {
+		setPasswordInput(event.currentTarget.value);
+	}
+
 	return (
 		<>
-			<Box>
-				<Heading>Login</Heading>
+			<Box
+				w="100vw"
+				h="100vh"
+				bgColor="gray.200"
+				display="flex"
+				flexDirection="column"
+				alignItems="center"
+				p={5}
+			>
+				<Heading>ShareCal Login</Heading>
+				<FormControl
+					bgColor="white"
+					w="50%"
+					p={5}
+					m={5}
+					borderRadius={5}
+				>
+                    <FormLabel>Email</FormLabel>
+                    <Input placeholder='Email' onChange={updateEmail} onBlur={updateEmail} isInvalid={emailError}/>
+					<Text pb={3} color="red.600">{emailMessage}</Text>
+                    <FormLabel>Password</FormLabel>
+                    <Input type="password" placeholder='Password' onChange={updatePassword} onBlur={updatePassword} isInvalid={passwordError}/>
+					<Text pb={3} color="red.600">{passwordMessage}</Text>
+                </FormControl>
+                <Button
+                    onClick={signIn}
+					bgColor="brand.300"
+					color="brand.200"
+					_hover={{
+						bgColor: "brand.200",
+						color: "brand.300",
+						borderColor: "brand.200"
+					}}
+					_focus={{
+						borderColor: "brand.300"
+					}}
+                >
+                    Log In
+                </Button>
 			</Box>
 		</>
 	);
--- client/src/theme/theme.js
+++ client/src/theme/theme.js
@@ -2,8 +2,15 @@ import { extendTheme } from "@chakra-ui/react";
 
 const theme = extendTheme({
 	fonts: {
-		heading: "Serif",
+		heading: "Sans-Serif",
 	},
+	colors: {
+		brand: {
+			100: "#000000",
+			200: "#FFFFFF",
+			300: "#0D968B"
+		}
+	}
 });
 
 export default theme;
