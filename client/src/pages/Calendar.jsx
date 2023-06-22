6e65411a13c100d6cc1f7e48ead0e8a3714c438e
Max Ohsawa <max.ohsawa@gmail.com>
2023-06-22 16:30:56 -0400

    adds link to fullcalendar.io react docs as a comment in Calendar.jsx page component

--- client/src/pages/Calendar.jsx
+++ client/src/pages/Calendar.jsx
@@ -3,6 +3,9 @@ import { Box, Heading } from "@chakra-ui/react";
 import FullCalendar from '@fullcalendar/react' // must go before plugins
 import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
 
+// fullcalendar.io react docs
+  // https://fullcalendar.io/docs/react
+
 function Calendar() {
 	return (
 		<>
