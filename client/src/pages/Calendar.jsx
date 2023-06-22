ac81d2011375f1bc2f9b5c0b4003dc1c086e4692
Max Ohsawa <max.ohsawa@gmail.com>
2023-06-22 17:05:34 -0400

    adds dateClick handler functionality to dynamically create new event on date clicked

--- client/src/pages/Calendar.jsx
+++ client/src/pages/Calendar.jsx
@@ -1,5 +1,10 @@
+// import react hooks
+import { useState } from 'react';
+
 // import box from chakra
 import { Box, Heading } from "@chakra-ui/react";
+
+// import fullcalendar adapter and plugins
 import FullCalendar from '@fullcalendar/react' // must go before plugins
 import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
 import interactionPlugin from "@fullcalendar/interaction" // needed for dayClick
@@ -8,17 +13,40 @@ import { handleDateClick } from "../utils/calendarHandlers";
 
 // fullcalendar.io react docs
   // https://fullcalendar.io/docs/react
+// additional calendar plugins
+  // https://fullcalendar.io/docs/plugin-index
+
+const wrapperStyle = {
+  width: "90vw",
+  height: "90vw"
+}
 
 function Calendar() {
+
+  const [events, setEvents ] = useState(
+    [
+      { title: 'test event 1', date: '2023-06-22' },
+      { title: 'test event 2', date: '2023-06-23' }
+    ]
+  )
+
 	return (
 		<>
 			<Box>
 				<Heading>Calendar</Heading>
-        <FullCalendar
-          plugins={[ dayGridPlugin, interactionPlugin ]}
-          initialView="dayGridMonth"
-          dateClick={handleDateClick}
-        />
+        
+        <div
+          style={wrapperStyle}
+        >
+
+          <FullCalendar
+            
+            plugins={[ dayGridPlugin, interactionPlugin ]}
+            initialView="dayGridMonth"
+            events={events}
+            dateClick={(args) => handleDateClick(args, events, setEvents)}
+          />
+        </div>
 			</Box>
 		</>
 	);
--- client/src/utils/calendarHandlers.js
+++ client/src/utils/calendarHandlers.js
@@ -1,4 +1,9 @@
-export const handleDateClick = (arg) => {
+export const handleDateClick = (arg, events, setEvents) => {
   console.log("date click event handling");
   console.log(arg);
+  const newEvent = {
+    title: 'New Event',
+    date: arg.dateStr
+  }
+  setEvents([...events, newEvent]);
 }
