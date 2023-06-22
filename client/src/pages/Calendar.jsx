5c580e9d4ae3abf7ebe74a9429ef6ec3fb0735c5
Max Ohsawa <max.ohsawa@gmail.com>
2023-06-22 16:27:26 -0400

    adds fullcalendar component into calendar page component

--- client/src/pages/Calendar.jsx
+++ client/src/pages/Calendar.jsx
@@ -1,11 +1,17 @@
 // import box from chakra
 import { Box, Heading } from "@chakra-ui/react";
+import FullCalendar from '@fullcalendar/react' // must go before plugins
+import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
 
 function Calendar() {
 	return (
 		<>
 			<Box>
 				<Heading>Calendar</Heading>
+        <FullCalendar
+          plugins={[ dayGridPlugin ]}
+          initialView="dayGridMonth"
+        />
 			</Box>
 		</>
 	);
