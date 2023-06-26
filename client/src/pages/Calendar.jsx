3a9f5731d061d75285a3e0c1468d7f0051611480
David Keim <dmk252@cornell.edu>
2023-06-26 12:07:45 -0400

    created header component

--- client/src/pages/Calendar.jsx
+++ client/src/pages/Calendar.jsx
@@ -2,6 +2,7 @@
 import { Box, Heading } from "@chakra-ui/react";
 import FullCalendar from '@fullcalendar/react' // must go before plugins
 import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
+import Header from "../components/Header.jsx";
 
 // fullcalendar.io react docs
   // https://fullcalendar.io/docs/react
@@ -9,12 +10,24 @@ import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
 function Calendar() {
 	return (
 		<>
-			<Box>
-				<Heading>Calendar</Heading>
-        <FullCalendar
-          plugins={[ dayGridPlugin ]}
-          initialView="dayGridMonth"
-        />
+			<Box
+				w="100vw"
+				h="100vh"
+				bgColor="gray.200"
+				display="flex"
+				flexDirection="column"
+				alignItems="center"
+			>
+				<Header />
+				<Heading
+					my={5}
+				>
+					Calendar
+				</Heading>
+				<FullCalendar
+				plugins={[ dayGridPlugin ]}
+				initialView="dayGridMonth"
+				/>
 			</Box>
 		</>
 	);
