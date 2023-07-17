bb3abbce143ac1d3eaec0d4e2f6fb390474d4540
Elias Spector-Zabusky <elias.spector.zabusky@gmail.com>
2023-07-17 14:50:30 -0400

    combines websocket and express server to one port. removes the need for websocket-beginning branches

--- server/server.js
+++ server/server.js
@@ -2,107 +2,104 @@
 // BEGIN WEBSOCKET CODE
 // **************************************
 
-// SETUP
-const { WebSocket, WebSocketServer } = require('ws')
-const uuidv4 = require('uuid').v4
-const http = require('http')
-const server = http.createServer()
-const wsServer = new WebSocketServer({ server })
-const port = 8000
-server.listen(port, () => {
-  console.log(`WebSocket server is running on port ${port}`)
-})
+// COMBINED SETUP
+const path = require("path");
+const express = require("express");
+const { createServer } = require("http");
+require("dotenv").config();
+
+const { WebSocket, WebSocketServer } = require("ws");
+const app = express();
+const server = createServer(app);
+const uuidv4 = require("uuid").v4;
+const wsServer = new WebSocketServer({ server });
+const PORT = process.env.PORT || 3001;
+
+// server.listen(port, () => {
+// 	console.log(`WebSocket server is running on port ${port}`);
+// });
 
 // object tracks client connections
-const clients = {}
+const clients = {};
 // object tracks users
-const users = {}
+const users = {};
 // user activity history
-const chatMessages = []
+const chatMessages = [];
 // event type definitions
 
 // HELPERS
 // broadcast
 function broadcastMessage(json) {
-
-  const data = JSON.stringify(json)
-  for (const userId in clients) {
-    const client = clients[userId]
-    if (client.readyState === WebSocket.OPEN) {
-      client.send(data)
-    }
-  }
+	const data = JSON.stringify(json);
+	for (const userId in clients) {
+		const client = clients[userId];
+		if (client.readyState === WebSocket.OPEN) {
+			client.send(data);
+		}
+	}
 }
 
 // message
 function handleMessage(message, userId) {
-  const dataFromClient = JSON.parse(message.toString())
-  const json = { type: dataFromClient.type }
+	const dataFromClient = JSON.parse(message.toString());
+	const json = { type: dataFromClient.type };
 
-  if (dataFromClient.type === 'userevent') {
+	if (dataFromClient.type === "userevent") {
+		users[userId] = dataFromClient;
+		chatMessages.push(`${dataFromClient.username} joined the chat`);
+		json.data = { users, chatMessages };
+	} else if (dataFromClient.type === "chatevent") {
+		chatMessages.push(`${dataFromClient.username}: ${dataFromClient.content}`);
+		editorContent = dataFromClient.content;
+		json.data = { editorContent, chatMessages };
+	}
 
-    users[userId] = dataFromClient
-    chatMessages.push(`${dataFromClient.username} joined the chat`)
-    json.data = { users, chatMessages }
-  } else if (dataFromClient.type === 'chatevent') {
-    chatMessages.push(`${dataFromClient.username}: ${dataFromClient.content}`)
-    editorContent = dataFromClient.content
-    json.data = { editorContent, chatMessages }
-  }
-
-  broadcastMessage(json)
+	broadcastMessage(json);
 }
 
 // websocket disconnect
 function handleDisconnect(userId) {
-  console.log(`${userId} disconnected`)
-  const json = { type: 'userevent' }
-  const username = users[userId]?.username || userId
-  chatMessages.push(`${username} left the chat`)
-  json.data = { users, chatMessages }
-  delete clients[userId]
-  delete users[userId]
-  broadcastMessage(json)
+	console.log(`${userId} disconnected`);
+	const json = { type: "userevent" };
+	const username = users[userId]?.username || userId;
+	chatMessages.push(`${username} left the chat`);
+	json.data = { users, chatMessages };
+	delete clients[userId];
+	delete users[userId];
+	broadcastMessage(json);
 }
 
 // websocket connection setup
-wsServer.on('connection', function(connection) {
-  // Generate a unique code for every user
-  const userId = uuidv4()
-  console.log('Recieved a new connection')
+wsServer.on("connection", function (connection) {
+	// Generate a unique code for every user
+	const userId = uuidv4();
+	console.log("Recieved a new connection");
 
-  // Store the new connection and handle messages
-  clients[userId] = connection
-  console.log(`${userId} connected.`)
-  connection.on('message', (message) => handleMessage(message, userId))
-  // User disconnected
-  connection.on('close', () => handleDisconnect(userId))
-})
+	// Store the new connection and handle messages
+	clients[userId] = connection;
+	console.log(`${userId} connected.`);
+	connection.on("message", (message) => handleMessage(message, userId));
+	// User disconnected
+	connection.on("close", () => handleDisconnect(userId));
+});
 
 // **************************************
 // END WEBSOCKET CODE
 // **************************************
 
-const path = require('path');
-const express = require('express');
-require('dotenv').config();
-const db = require('./db/connection');
-
-const routes = require('./routes');
-
-const PORT = process.env.PORT || 3001;
-
-const app = express();
+const db = require("./db/connection");
+const routes = require("./routes");
+const { create } = require("./models/User");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, '..', 'client', 'public')));
+app.use(express.static(path.join(__dirname, "..", "client", "public")));
 
-app.use('/', routes);
+app.use("/", routes);
 
-db.once('open', () => {
-  console.log('DB connection established')
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
+db.once("open", () => {
+	console.log("DB connection established");
+	server.listen(PORT, () => {
+		console.log(`Server running on port ${PORT}`);
+	});
 });
